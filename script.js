567ddf715fa71d3a1d724dd24dea5d6e8861e3ea
Mohibullah Arsala <mohib-arsala@Mohibullahs-MacBook-Pro.local>
2020-07-28 18:05:02 -0400

    write and delete added and explained in node js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 
 
+// Async
+
 fs.readFile("./app.txt", (err, data) => {
     if(err) {
         console.log("error");
@@ -8,12 +10,34 @@ fs.readFile("./app.txt", (err, data) => {
     console.log("Async", data.toString("utf8"));
 });
 
+// Sync
+
 const file = fs.readFileSync("./app.txt");
 console.log("Sync", file.toString());
 
-fs.appendFile("./app.txt", " This is good!! like the way it is working", err => {
+
+//Append
+
+/*fs.appendFile("./app.txt", " This is good!! like the way it is working", err => {
     if(err) {
         console.log("Not found, Someting worng!");
     }
-});
+});*/
 
+
+// Write 
+
+/*fs.writeFile("W.txt", " heppy to Create or write a new file!", err=> {
+    if(err){
+        console.log("Error, Something gose wrong!");
+    }
+});*/
+
+// Delete
+
+fs.unlink("./W.txt", err=> {
+    if(err){
+        console.log(err);
+    }
+    console.log("Inception");
+})
