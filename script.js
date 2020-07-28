1de287397934e867d2d7fff9a54d5cf298a7cccf
Mohibullah Arsala <mohib-arsala@Mohibullahs-MacBook-Pro.local>
2020-07-28 17:54:17 -0400

    update appendFile added

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ fs.readFile("./app.txt", (err, data) => {
 
 const file = fs.readFileSync("./app.txt");
 console.log("Sync", file.toString());
+
+fs.appendFile("./app.txt", " This is good!! like the way it is working", err => {
+    if(err) {
+        console.log("Not found, Someting worng!");
+    }
+});
+
