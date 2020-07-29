52879803152260670e16a0dca7f9d80da79ab979
Mohibullah Arsala <mohib-arsala@Mohibullahs-MacBook-Pro.local>
2020-07-29 15:53:29 -0400

    file added

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ const fs = require("fs");
 
 // Async
 
-fs.readFile("./app.txt", (err, data) => {
+/*fs.readFile("./app.txt", (err, data) => {
     if(err) {
         console.log("error");
     }
     console.log("Async", data.toString("utf8"));
-});
+});*/
 
 // Sync
 
-const file = fs.readFileSync("./app.txt");
-console.log("Sync", file.toString());
+/*const file = fs.readFileSync("./app.txt");
+console.log("Sync", file.toString());*/
 
 
 //Append
@@ -35,9 +35,50 @@ console.log("Sync", file.toString());
 
 // Delete
 
-fs.unlink("./W.txt", err=> {
+/*fs.unlink("./W.txt", err=> {
     if(err){
         console.log(err);
     }
     console.log("Inception");
-})
+})*/
+question1 = () => {
+    fs.readFile("./sunta.txt", (err, data)=> {
+        console.time("Q1 = Sunta-time");
+        const directions = data.toString();
+        const directionsArray = directions.split('');
+        const answer = directionsArray.reduce((acc, currentValue) => {
+           if(currentValue === "(") {
+               return acc += 1
+           } else if (currentValue === ")") {
+               return acc -=1
+           }
+        }, 0)
+        console.timeEnd("Q1 = Sunta-time");
+        console.log("floor: ", answer);
+    })
+}
+
+question1();
+
+ question2 = () => {
+    fs.readFile("./sunta.txt", (err, data)=> {
+        console.time("Q2 = Sunta-time");
+        const directions = data.toString();
+        const directionsArray = directions.split('');
+        let accumulator = 0;
+        let counter = 0;
+        const aswer = directionsArray.some((currentItem)=> {
+            if(currentItem === "(") {
+                accumulator += 1
+            } else if (currentItem === ")") {
+                accumulator -= 1
+            }
+            counter ++
+            return accumulator < 0;
+        });
+        console.timeEnd("Q2 = Sunta-time");
+        console.log("Basement Entered at: ", counter);
+    });
+};
+
+question2();
